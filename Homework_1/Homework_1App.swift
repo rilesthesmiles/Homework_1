5d44422fa69ec67172dc744c304db189b29f353c
Riley_Marshall <rileyrmarshall1@gmail.com>
2023-09-05 16:05:27 -0600

    Initial Commit

---
+++ Homework_1/Homework_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_1App.swift
+//  Homework_1
+//
+//  Created by Riley Marshall on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Homework_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
